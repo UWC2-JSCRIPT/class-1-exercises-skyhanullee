aacc2cd2b2a2c8fdf30f6a18d4cb566a9f6234f2
skyhanullee <leeskyha@gmail.com>
2022-10-20 05:35:50 -0700

    first changes

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -5,19 +5,39 @@
 // 1. Pagliacci charges $16.99 for a 13” pizza and $19.99 for a 17” pizza.
 // What is the area for each of these pizzas?
 // (radius would be the listed size - i.e. 13" - divided by 2)
+const SMALL_PIZZA_DIAMETER = 13;
+const LARGE_PIZZA_DIAMETER = 17;
+
+const SMALL_PIZZA_COST = 16.99;
+const LARGE_PIZZA_COST = 19.99;
+
+const smallPizzaArea = Math.PI * ((SMALL_PIZZA_DIAMETER / 2)**2);
+const largePizzaArea = Math.PI * ((SMALL_PIZZA_DIAMETER / 2)**2);
 
 
 // 2. What is the cost per square inch of each pizza?
+const smallCostPerSquareInch = SMALL_PIZZA_COST / smallPizzaArea;
+const largeCostPerSquareInch = LARGE_PIZZA_COST / largePizzaArea;
+
 
 
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
+const card = Math.ceil(Math.random() * 13);
 
 
 // 4. Draw 3 cards and use Math to determine the highest
 // card
 
+let cards = [];
+
+for (let i = 0; i < 3; i++) {
+    let singleCard = Math.ceil(Math.random() * 13);
+    cards.push(singleCard);
+}
+
+const highestCard = Math.max(...cards);
 
 /**
  * ADDRESS LINE
@@ -27,6 +47,13 @@
 // streetAddress, city, state, and zipCode. Use
 // this information to create a formatted address block
 // that could be printed onto an envelope.
+
+const firstName = 'John';
+const lastName = 'Doe';
+const streetAddress = '123 Main Street';
+const city = 'New York';
+const state = 'NY';
+const zipCode = '10001';
 
 
 // 2. You are given a string in this format:
